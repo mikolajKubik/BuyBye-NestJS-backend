db723fed7de8929593c10d9b2a9489fe779c9766
Mikołaj Kubik <mikolaj.k.kubik@gmail.com>
2024-10-29 23:15:41 +0100

    .

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,6 +1,6 @@
 import { Optional } from "@nestjs/common";
 import { Type } from "class-transformer";
-import { IsArray, IsDateString, IsEmail, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, IsUUID, MaxLength, Min, MinLength, ValidateNested } from "class-validator";
+import { IsArray, IsDateString, IsEmail, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, IsUUID, Matches, MaxLength, Min, MinLength, ValidateNested } from "class-validator";
 import { CreateProductOrderDto } from "src/product-order/dto/create-product-order.dto";
 
 export class CreateOrderDto {
@@ -26,7 +26,10 @@ export class CreateOrderDto {
 
     @MaxLength(50) 
     @MinLength(1)
-    @IsPhoneNumber('PL')
+    @IsString()
+    @MinLength(1)
+    @MaxLength(50)
+    @Matches(/^\d+$/, { message: 'Phone number can contain only numbers' })
     phone: string;
 
     @IsArray()
