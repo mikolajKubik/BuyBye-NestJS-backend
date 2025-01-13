4db523a6d59865d27acb1d55731f0412def23e54
Mikołaj Kubik <mikolaj.k.kubik@gmail.com>
2025-01-13 16:22:14 +0100

    disabled protection

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new ValidationFilter());
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
